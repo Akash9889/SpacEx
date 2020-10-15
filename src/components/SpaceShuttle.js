bc51987e2e03b0b9c3a89cb690b4b03f98c73e70
Your Name <you@example.com>
2020-10-15 17:44:58 +0530

    css

--- src/components/SpaceShuttle.js
+++ src/components/SpaceShuttle.js
@@ -6,17 +6,17 @@ export default function SpaceShuttle({sample}) {
             <img id = 'mission-patch' src = {sample.links.mission_patch_small} alt = 'mission-patch'/>
             <span className= 'box-contents'>
                 <p>{`${sample.rocket.rocket_name} #${sample.flight_number}`}</p>
-                <p>Mission id: {sample.mission_id}</p>
-                <p>Launch year: {sample.launch_year}</p>
-                <p>Successfull launching:{
+                <p>Mission id: <span>{sample.mission_id}</span></p>
+                <p>Launch year:  <span>{sample.launch_year}</span></p>
+                <p>Successfull launching:<span>{
                     !sample.launch_success ? 
                     sample.launch_success === null ? 'NA' : 'False' : 'True' 
-                    }
+                    }</span>
                 </p>
-                <p>Successfull landing: {
+                <p>Successfull landing: <span>{
                     !sample.rocket.first_stage.cores[0].land_success ?
                     sample.rocket.first_stage.cores[0].land_success === null ? 'NA' : 'False': 'True' 
-                    }
+                    }</span>
                 </p>
             </span>
         </>
